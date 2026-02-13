d7a08bf7497adf2314892fb876fddfea2797bd17
axediant12 <ianfredcaballero@gail.com>
2026-02-13 09:56:23 +0800

    updated database.php for database config

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$db   = 'barangay-appointment';
-$user = 'root';
-$pass = '';
+$host = 'db.fr-pari1.bengt.wasmernet.com';
+$db   = 'dbJuJUVJFKkdfuLPao8bReZG';
+$user = 'e844979b7e6780004a041c356833';
+$pass = '0698e844-979b-7f79-8000-ac0db617c008';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
